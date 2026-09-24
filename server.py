a2f79c40f595faeff28c2ed2ca5e3702c4f2251f
Rockey-Jason <jason.yeonwoo.im@gmail.com>
2026-09-24 14:07:34 +0900

    Delete server.py

--- server.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ.setdefault('DORI_HOST','0.0.0.0')
-from dori_server import H, ThreadingHTTPServer
-host=os.getenv('DORI_HOST','0.0.0.0'); port=int(os.getenv('PORT',os.getenv('DORI_PORT','8000')))
-print(f'Dori AI server listening on {host}:{port}',flush=True); ThreadingHTTPServer((host,port),H).serve_forever()
